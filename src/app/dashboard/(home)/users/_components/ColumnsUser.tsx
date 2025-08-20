3cbb6bae7880bd7c0e1f27ae30869ae060a0942b
agamlatiff <agam.latiff@gmail.com>
2025-08-20 23:38:29 +0700

    Dashboard - Users (1)

---
+++ src/app/dashboard/(home)/users/_components/ColumnsUser.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import type { User } from "@prisma/client";
+import type { ColumnDef } from "@tanstack/react-table";
+
+export const columns: ColumnDef<User>[] = [
+  { accessorKey: "name", header: "Name" },
+  { accessorKey: "email", header: "Email" },
+  { accessorKey: "passport", header: "Passport" },
+];
---
+++ src/app/dashboard/(home)/users/page.tsx
@@ -0,0 +1,23 @@
+import { DataTable } from "@/components/ui/data-table";
+import type { Metadata } from "next";
+import { columns } from "./_components/ColumnsUser";
+
+
+export const metadata: Metadata = {
+  title: "Dashboard | User",
+};
+
+const UserPage = async () => {
+  const Users = await getUsers()
+
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between">
+        <div className="my-5 text-2xl font-bold">User</div>
+      </div>
+      <DataTable columns={columns} data={Users} />
+    </>
+  );
+};
+
+export default UserPage;
