35671a98c1a3fc8261e827883b8870612b276361
agamlatiff <agam.latiff@gmail.com>
2025-08-20 23:32:37 +0700

    Dashboard - Tickets (1)

---
+++ src/app/dashboard/(home)/tickets/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { columns } from "./_components/ColumnsTicket";
+import { DataTable } from "@/components/ui/data-table";
+
+export const metadata: Metadata = {
+  title: "Dashboard | Ticket",
+};
+
+const TicketPage = async () => {
+  // const data = await getTicket()
+
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between">
+        <div className="my-5 text-2xl font-bold">Ticket</div>
+      </div>
+      <DataTable columns={columns} data={[]} />
+    </>
+  );
+};
+
+export default TicketPage;
