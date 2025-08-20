d217aadff3ee43ef0a1c6f81b4620899b00ab569
agamlatiff <agam.latiff@gmail.com>
2025-08-20 09:45:06 +0700

    Dashboard - Airplanes (9)

--- src/app/dashboard/(home)/airplanes/lib/actions.ts
+++ src/app/dashboard/(home)/airplanes/lib/actions.ts
@@ -57,11 +57,91 @@ export async function saveAirplane(
         image: uploadedFile as string,
       },
     });
+    console.log(data);
   } catch (e) {
     console.log(e);
     return {
       errorTitle: "Failed to save airplane",
       errorDesc: ["Database Error", "Please try again later"],
+    };
+  }
+
+  revalidatePath("/dashboard/airplanes");
+  return redirect("/dashboard/airplanes");
+}
+
+export async function updateAirplane(
+  _: unknown,
+  formData: FormData,
+  id: string
+): Promise<ActionResult> {
+  const image = formData.get("image") as File;
+
+  let airplaneFormSchemaUpdate;
+
+  if (image.size === 0) {
+    airplaneFormSchemaUpdate = airplaneFormSchema.omit({ image: true });
+  } else {
+    airplaneFormSchemaUpdate = airplaneFormSchema;
+  }
+
+  const values = airplaneFormSchemaUpdate.safeParse({
+    name: formData.get("name"),
+    code: formData.get("code"),
+    image: formData.get("image"),
+  });
+
+  if (!values.success) {
+    const errorDesc = values.error.issues.map((issue) => issue.message);
+
+    return {
+      errorTitle: "Validation Error",
+      errorDesc,
+    };
+  }
+
+  let filename : unknown;
+
+  if (image.size > 0) {
+    const uploadedFile = await uploadFile(image);
+
+    if (uploadedFile instanceof Error) {
+      return {
+        errorTitle: "Failed to upload file",
+        errorDesc: ["Connection Error", "Please try again later"],
+      };
+    }
+
+    filename = uploadedFile as string;
+  } else {
+    const airplane = await prisma.airplane.findFirst({
+      where: {
+        id,
+      },
+      select: {
+        image: true,
+      },
+    });
+
+    filename = airplane?.image;
+  }
+
+  try {
+    await prisma.airplane.update({
+      where: {
+        id,
+      },
+      data: {
+        name: values.data.name,
+        code: values.data.code,
+        image: filename as string,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    return {
+      errorTitle: "Failed to update file",
+      errorDesc: ["Connection Error", "Please try again later"],
     };
   }
 
