bcd9ce7ae07ac1a705f3a0aaa828ff11b6779b4a
agamlatiff <agam.latiff@gmail.com>
2025-08-21 14:26:48 +0700

    Landing Page - Available Flights (4)

--- src/app/(home)/available-flights/_components/FilterClass.tsx
+++ src/app/(home)/available-flights/_components/FilterClass.tsx
@@ -1,45 +1,26 @@
-import React from "react";
+import type { TypeSeat } from "@prisma/client";
+
+const SEAT_OPTIONS: TypeSeat[] = ["ECONOMY", "BUSSINESS", "FIRST"];
 
 const FilterClass = () => {
   return (
     <div className="flex flex-col gap-4">
       <p className="font-semibold">Seat Class</p>
-      <label
-        htmlFor="economy"
-        className="font-semibold flex items-center gap-[10px] has-[:checked]:text-white"
-      >
-        <input
-          type="radio"
-          name="seat"
-          id="economy"
-          className="w-[18px] h-[18px] appearance-none checked:border-[3px] checked:border-solid checked:border-flysha-black rounded-full checked:bg-flysha-light-purple ring-2 ring-flysha-off-purple checked:ring-white"
-        />
-        Economy
-      </label>
-      <label
-        htmlFor="business"
-        className="font-semibold flex items-center gap-[10px] has-[:checked]:text-white"
-      >
-        <input
-          type="radio"
-          name="seat"
-          id="business"
-          className="w-[18px] h-[18px] appearance-none checked:border-[3px] checked:border-solid checked:border-flysha-black rounded-full checked:bg-flysha-light-purple ring-2 ring-flysha-off-purple checked:ring-white"
-        />
-        Business
-      </label>
-      <label
-        htmlFor="first"
-        className="font-semibold flex items-center gap-[10px] has-[:checked]:text-white"
-      >
-        <input
-          type="radio"
-          name="seat"
-          id="first"
-          className="w-[18px] h-[18px] appearance-none checked:border-[3px] checked:border-solid checked:border-flysha-black rounded-full checked:bg-flysha-light-purple ring-2 ring-flysha-off-purple checked:ring-white"
-        />
-        First
-      </label>
+      {SEAT_OPTIONS.map((item, index) => (
+        <label key={`${item + index}`}
+          htmlFor={item}
+          className="font-semibold flex items-center gap-[10px] has-[:checked]:text-white"
+        >
+          <input
+            type="radio"
+            name="seat"
+            id={item}
+            value={item}
+            className="w-[18px] h-[18px] appearance-none checked:border-[3px] checked:border-solid checked:border-flysha-black rounded-full checked:bg-flysha-light-purple ring-2 ring-flysha-off-purple checked:ring-white"
+          />
+          {item}
+        </label>
+      ))}
     </div>
   );
 };
