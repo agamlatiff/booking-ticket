e7d71233b8bdefb3d050080ccc6ecadeafaae81f
agamlatiff <agam.latiff@gmail.com>
2025-08-25 19:56:28 +0700

    Landing Page - Checkout (1)

--- src/app/(home)/available-flights/_components/FilterClass.tsx
+++ src/app/(home)/available-flights/_components/FilterClass.tsx
@@ -19,29 +19,29 @@ const FilterClass = () => {
         planeId: "",
       },
     });
-
-    return (
-      <div className="flex flex-col gap-4">
-        <p className="font-semibold">Seat Class</p>
-        {SEAT_OPTIONS.map((item, index) => (
-          <label
-            key={`${item + index}`}
-            htmlFor={item}
-            className="font-semibold flex items-center gap-[10px] has-[:checked]:text-white"
-          >
-            <input
-              onChange={handleChange}
-              type="radio"
-              name="seat"
-              id={item}
-              value={item}
-              className="w-[18px] h-[18px] appearance-none checked:border-[3px] checked:border-solid checked:border-flysha-black rounded-full checked:bg-flysha-light-purple ring-2 ring-flysha-off-purple checked:ring-white"
-            />
-            {item}
-          </label>
-        ))}
-      </div>
-    );
   };
+  
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="font-semibold">Seat Class</p>
+      {SEAT_OPTIONS.map((item, index) => (
+        <label
+          key={`${item + index}`}
+          htmlFor={item}
+          className="font-semibold flex items-center gap-[10px] has-[:checked]:text-white"
+        >
+          <input
+            onChange={handleChange}
+            type="radio"
+            name="seat"
+            id={item}
+            value={item}
+            className="w-[18px] h-[18px] appearance-none checked:border-[3px] checked:border-solid checked:border-flysha-black rounded-full checked:bg-flysha-light-purple ring-2 ring-flysha-off-purple checked:ring-white"
+          />
+          {item}
+        </label>
+      ))}
+    </div>
+  );
 };
 export default FilterClass;
